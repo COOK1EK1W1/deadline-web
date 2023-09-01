9dc6bdb815c1f5e7f12ccfdd6bf7968f44d21940
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2023-09-01 13:03:22 +0100

    added create deadline button

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -1,7 +1,14 @@
+"use client"
 import { useState } from "react"
 import DateIco from "./date"
+import { PiNotePencilBold } from "react-icons/pi"
 
 export default function modal({deadlines, today} : {deadlines: (Deadline | undefined)[], today:Date}){
+  const [showNewForm, setShowNewForm] = useState(false);
+  function openNewForm(){
+    setShowNewForm(true);
+  }
+
   const [formData, setFormData] = useState<Deadline>({
     name: '',
     subject: '',
@@ -67,87 +74,95 @@ export default function modal({deadlines, today} : {deadlines: (Deadline | undef
     
   }
   return <div className="m-2" onClick={(e)=>{e.stopPropagation()}}>
-    <DateIco date={today} showDay={false}/>
+    <div className="flex justify-between">
+      <DateIco date={today} showDay={false}/>
+    
+      <button type="button" className="bg-green-300 hover:bg-blue-400 rounded-full m-1 p-1 w-40" onClick={openNewForm}>
+        <PiNotePencilBold style={{display: "inline"}} /> Create New
+        </button>
+    </div>
     <div className="h-[53vh] overflow-y-auto">
+    
+      
+        {showNewForm && <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="subject">Subject</label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="startDate">Start Date</label>
+            <input
+              type="datetime-local"
+              id="start"
+              name="start"
+              value={formData.start}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="dueDate">Due Date</label>
+            <input
+              type="datetime-local"
+              id="due"
+              name="due"
+              value={formData.due}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="mark">Mark</label>
+            <input
+              type="number"
+              id="mark"
+              name="mark"
+              value={formData.mark}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="info">Info</label>
+            <textarea
+              id="info"
+              name="info"
+              value={formData.info}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="url">url</label>
+            <input
+              type="url"
+              id="url"
+              name="url"
+              value={formData.url}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>}
+
       {deadlineRows}
-      {(deadlineRows.length == 0) && (
-      <div>no deadlines 
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded px-4">create new</button>
-        <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="subject">Subject</label>
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="startDate">Start Date</label>
-        <input
-          type="datetime-local"
-          id="start"
-          name="start"
-          value={formData.start}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="dueDate">Due Date</label>
-        <input
-          type="datetime-local"
-          id="due"
-          name="due"
-          value={formData.due}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="mark">Mark</label>
-        <input
-          type="number"
-          id="mark"
-          name="mark"
-          value={formData.mark}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="info">Info</label>
-        <textarea
-          id="info"
-          name="info"
-          value={formData.info}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="url">url</label>
-        <input
-          type="url"
-          id="url"
-          name="url"
-          value={formData.url}
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-      </div>)}
+
+      {(deadlineRows.length == 0) && !showNewForm && (
+      <div className="flex justify-center p-8">No Deadlines!!</div>)}
     </div>
     
   </div>
