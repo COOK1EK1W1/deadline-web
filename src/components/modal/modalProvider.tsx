30aeba3a40b42ffe66d3617b711d6e4ceee6fba2
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2023-09-19 23:18:51 +0100

    improved types within modalProvider

--- src/components/modal/modalProvider.tsx
+++ src/components/modal/modalProvider.tsx
@@ -2,32 +2,37 @@ import { Deadline } from "@prisma/client"
 import React, { ReactNode, useCallback, useMemo } from "react"
 import { useState } from "react"
 
+type DeadlineDate = {
+  date: Date,
+  deadlines: (Deadline | null)[]
+}
+
 type ModalDataContext = {
-  showCover: boolean, 
-  showModal: boolean, 
-  modalDeadlines: {date: Date, deadlines: ( Deadline|null)[]}, 
+  showCover: boolean,
+  showModal: boolean,
+  modalDeadlines: DeadlineDate,
 }
 
 type ModalMutatorContext2 = {
-  openModal: (d: { date: Date, deadlines: (Deadline | null)[]}) => void, 
-  closeModal: () => void, 
+  openModal: (d: DeadlineDate) => void,
+  closeModal: () => void,
 }
 
 export const ContextData = React.createContext<ModalDataContext>(
   { showCover: false, showModal: false, modalDeadlines: { date: new Date(), deadlines: [] } }
-  )
+)
 export const ContextMutator = React.createContext<ModalMutatorContext2>({ openModal: () => { }, closeModal: () => { } })
 
-export function ModalProvider({children, modal}:{children:ReactNode, modal: ReactNode}) {
+export function ModalProvider({ children, modal }: { children: ReactNode, modal: ReactNode }) {
 
-  const [modalDeadlines, setModalDeadlines] = useState<{ date: Date, deadlines: (Deadline | null)[] }>({ date: new Date(), deadlines: [] })
+  const [modalDeadlines, setModalDeadlines] = useState<DeadlineDate>({ date: new Date(), deadlines: [] })
 
   const [showModal, setShowModal] = useState(false)
   const [showCover, setshowCover] = useState(false)
 
   // console.log("modal provider render");
 
-  const openModal = useCallback((d: { date: Date, deadlines: (Deadline | null)[]; }) => {
+  const openModal = useCallback((d: DeadlineDate) => {
     setModalDeadlines(d);
     setshowCover(true);
     setTimeout(() => setShowModal(true), 10);
