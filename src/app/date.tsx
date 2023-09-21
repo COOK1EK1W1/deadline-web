7ef32fa1c85f0d278af10aedc4b5006a240bdb06
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2023-09-21 09:18:31 +0100

    converted most stuff to date-fns

--- src/app/date.tsx
+++ src/app/date.tsx
@@ -1,7 +1,7 @@
+import { format, isToday } from "date-fns"
+
 export default function DateIco({date, showDay}: {date: Date, showDay: boolean}){
-  const months = ["", "Jan", "Feb", "Mar", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
-  return <div className={`${date.getDate() == 1 && !showDay ? "w-24" : date.getDate() == 1 || !showDay ? "w-20": "w-8"} m-1 p-1 text-center rounded-full ${(date.toLocaleDateString() == new Date().toLocaleDateString()) ? "bg-red-500" :  "bg-slate-300 dark:bg-slate-600" }`}>
-                {!showDay && days[date.getDay()]} {date.getDate()} {date.getDate() == 1 && months[date.getMonth()]}
+  return <div className={`${date.getDate() == 1 && !showDay ? "w-24" : date.getDate() == 1 || !showDay ? "w-20": "w-8"} m-1 p-1 text-center rounded-full ${isToday(date) ? "bg-red-500" :  "bg-slate-300 dark:bg-slate-600" }`}>
+                {!showDay && format(date, "eee")} {date.getDate()} {date.getDate() == 1 && format(date, "MMM")}
   </div>
 }
