1cb049059635fc61a6f824f5618bdc1a3cd50772
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-08-22 18:50:49 +0100

    cicd?

--- src/app/api/d_bound/[guild_id]/route.ts
+++ src/app/api/d_bound/[guild_id]/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server"
 
 
 export async function GET(request: Request, {params}: {params: {guild_id: string}}) {
-
   const code = await discordBound(params.guild_id)
   const deadlines = await getDeadlinesCache(code)
   return NextResponse.json(deadlines)
